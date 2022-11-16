987e97cdd524a0934ac2db13bdf8d73ac464be50
Lê Trần Hữu Danh <35548188+letranhuudanh0030@users.noreply.github.com>
2022-11-16 23:05:06 +0700

    setup admin dashboard v2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -50,7 +50,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->name('admin.')
                 ->prefix('admin')
-                ->group(base_path('routes.admin.php'));
+                ->group(base_path('routes/admin.php'));
         });
     }
 
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
